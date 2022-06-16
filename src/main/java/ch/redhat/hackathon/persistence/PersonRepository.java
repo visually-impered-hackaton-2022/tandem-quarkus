970b80edb358a185bafef1f43837e131c65ed31e
Nikolaj Majorov <nikolaj@majorov.biz>
2022-06-16 15:18:54 +0200

    optimized import

--- src/main/java/ch/redhat/hackathon/persistence/PersonRepository.java
+++ src/main/java/ch/redhat/hackathon/persistence/PersonRepository.java
@@ -1,14 +1,9 @@
 package ch.redhat.hackathon.persistence;
 
-import javax.enterprise.context.ApplicationScoped;
-
 import ch.redhat.hackathon.domain.Person;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import io.quarkus.hibernate.orm.panache.runtime.JpaOperations;
-import io.quarkus.panache.common.Sort;
-import io.smallrye.mutiny.Uni;
 
-import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class PersonRepository implements PanacheRepository<Person> {
